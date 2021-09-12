ec856780e1411474d5bedf52736b5559b31c53d9
Jeison Ramos <juan.posso@yuxiglobal.com>
2021-09-12 03:31:08 -0500

    Add Bogota forecast

--- controler.js
+++ controler.js
@@ -10,8 +10,7 @@ window.addEventListener("load", function () {
   const exclude = "minutely,hourly,current";
   const lat = 4.6097;
   const lon = -74.0817;
-
-  let temperatureBgt = 0;
+  const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   const api_url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city_name1}&appid=${api_key}&units=${units}`;
   const api_url2 = `https://api.openweathermap.org/data/2.5/weather?q=${city_name2}&appid=${api_key}&units=${units}`;
@@ -25,13 +24,49 @@ window.addEventListener("load", function () {
   xhr_weather_paris.open("GET", api_url2);
   xhr_weather_paris.send();
 
-  xhr_forecast_bogota.addEventListener("load", onRequetHandler);
+  xhr_forecast_bogota.addEventListener("load", onRequetHandlerBGTForecast);
   xhr_forecast_bogota.open("GET", api_forecast);
   xhr_forecast_bogota.send();
 
-  function onRequetHandler() {
+  function onRequetHandlerBGTForecast() {
     if (this.readyState === 4 && this.status === 200) {
-      console.log(JSON.parse(this.response));
+      const response = JSON.parse(this.response);
+
+      var tableRef = document.getElementById("table-forecast");
+
+      var currentTime = new Date();
+      var newDate = new Date();
+
+      for (let i = response.daily.length - 1; i >= 0; i--) {
+        var newRow = tableRef.insertRow(0);
+        newRow.className = "row-forecast";
+
+        newDate.setDate(currentTime.getDate() + i);
+        const weekDay = newDate.getDay();
+        const day = currentTime.getDate() + i;
+        const month = currentTime.getMonth() + 1;
+        const year = currentTime.getFullYear();
+        const date = day + "/" + month + "/" + year;
+        const str = response.daily[i].temp.min + "° / " + response.daily[i].temp.max + "°";
+
+        var newCell1 = newRow.insertCell(0);
+        var newText1 = document.createTextNode(weekDays[weekDay] + ",  " + date);
+        newCell1.appendChild(newText1);
+
+        var img = document.createElement("img");
+        img.src = `./img/icons/${response.daily[i].weather[0].icon}.png`;
+        img.className = "img-forecast";
+        var newCell2 = newRow.insertCell(1);
+        newCell2.appendChild(img);
+
+        var newCell3 = newRow.insertCell(2);
+        var newText3 = document.createTextNode(str);
+        newCell3.appendChild(newText3);
+
+        var newCell4 = newRow.insertCell(3);
+        var newText4 = document.createTextNode(response.daily[i].weather[0].description.charAt(0).toUpperCase() + response.daily[i].weather[0].description.slice(1));
+        newCell4.appendChild(newText4);
+      }
     }
   }
 
@@ -39,10 +74,10 @@ window.addEventListener("load", function () {
     if (this.readyState === 4 && this.status === 200) {
       const response = JSON.parse(this.response);
       this.temperatureBgt = response.main.temp;
-      console.log(JSON.parse(this.temperatureBgt));
 
       document.getElementById("temperature-bgt").innerHTML = "-  " + response.main.temp + "°";
-      document.getElementById("tpm-description-bgt").innerHTML = response.weather[0].description;
+      document.getElementById("tpm-description-bgt").innerHTML =
+        response.weather[0].description.charAt(0).toUpperCase() + response.weather[0].description.slice(1);
 
       var img = document.createElement("img");
       img.src = `./img/icons/${response.weather[0].icon}.png`;
@@ -54,10 +89,10 @@ window.addEventListener("load", function () {
     if (this.readyState === 4 && this.status === 200) {
       const response = JSON.parse(this.response);
       this.temperatureBgt = response.main.temp;
-      console.log(JSON.parse(this.temperatureBgt));
 
       document.getElementById("temperature-paris").innerHTML = "-  " + response.main.temp + "°";
-      document.getElementById("tpm-description-paris").innerHTML = response.weather[0].description;
+      document.getElementById("tpm-description-paris").innerHTML =
+        response.weather[0].description.charAt(0).toUpperCase() + response.weather[0].description.slice(1);
 
       var img = document.createElement("img");
       img.src = `./img/icons/${response.weather[0].icon}.png`;
