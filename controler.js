031357407b7d0dd2cb48c3c7adf4d70289631825
Jeison Ramos <juan.posso@yuxiglobal.com>
2021-09-12 12:15:39 -0500

    clean code

--- controler.js
+++ controler.js
@@ -12,16 +12,16 @@ window.addEventListener("load", function () {
   const lon = -74.0817;
   const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  const api_url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city_name1}&appid=${api_key}&units=${units}`;
-  const api_url2 = `https://api.openweathermap.org/data/2.5/weather?q=${city_name2}&appid=${api_key}&units=${units}`;
+  const api_url_bgt = `https://api.openweathermap.org/data/2.5/weather?q=${city_name1}&appid=${api_key}&units=${units}`;
+  const api_url_paris = `https://api.openweathermap.org/data/2.5/weather?q=${city_name2}&appid=${api_key}&units=${units}`;
   const api_forecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}4&exclude=${exclude}&appid=${api_key}&units=${units}`;
 
   xhr_weather_bogota.addEventListener("load", onRequetHandlerBGT);
-  xhr_weather_bogota.open("GET", api_url1);
+  xhr_weather_bogota.open("GET", api_url_bgt);
   xhr_weather_bogota.send();
 
   xhr_weather_paris.addEventListener("load", onRequetHandlerParis);
-  xhr_weather_paris.open("GET", api_url2);
+  xhr_weather_paris.open("GET", api_url_paris);
   xhr_weather_paris.send();
 
   xhr_forecast_bogota.addEventListener("load", onRequetHandlerBGTForecast);
@@ -31,16 +31,13 @@ window.addEventListener("load", function () {
   function onRequetHandlerBGTForecast() {
     if (this.readyState === 4 && this.status === 200) {
       const response = JSON.parse(this.response);
-
-      var tableRef = document.getElementById("table-forecast");
-
-      var currentTime = new Date();
-      var newDate = new Date();
+      let tableRef = document.getElementById("table-forecast");
+      let currentTime = new Date();
+      let newDate = new Date();
 
       for (let i = response.daily.length - 1; i >= 0; i--) {
-        var newRow = tableRef.insertRow(0);
+        let newRow = tableRef.insertRow(0);
         newRow.className = "row-forecast";
-
         newDate.setDate(currentTime.getDate() + i);
         const weekDay = newDate.getDay();
         const day = currentTime.getDate() + i;
@@ -49,22 +46,22 @@ window.addEventListener("load", function () {
         const date = day + "/" + month + "/" + year;
         const str = response.daily[i].temp.min + "° / " + response.daily[i].temp.max + "°";
 
-        var newCell1 = newRow.insertCell(0);
-        var newText1 = document.createTextNode(weekDays[weekDay] + ",  " + date);
+        let newCell1 = newRow.insertCell(0);
+        let newText1 = document.createTextNode(weekDays[weekDay] + ",  " + date);
         newCell1.appendChild(newText1);
 
-        var img = document.createElement("img");
+        let img = document.createElement("img");
         img.src = `./img/icons/${response.daily[i].weather[0].icon}.png`;
         img.className = "img-forecast";
-        var newCell2 = newRow.insertCell(1);
+        let newCell2 = newRow.insertCell(1);
         newCell2.appendChild(img);
 
-        var newCell3 = newRow.insertCell(2);
-        var newText3 = document.createTextNode(str);
+        let newCell3 = newRow.insertCell(2);
+        let newText3 = document.createTextNode(str);
         newCell3.appendChild(newText3);
 
-        var newCell4 = newRow.insertCell(3);
-        var newText4 = document.createTextNode(response.daily[i].weather[0].description.charAt(0).toUpperCase() + response.daily[i].weather[0].description.slice(1));
+        let newCell4 = newRow.insertCell(3);
+        let newText4 = document.createTextNode(response.daily[i].weather[0].description.charAt(0).toUpperCase() + response.daily[i].weather[0].description.slice(1));
         newCell4.appendChild(newText4);
       }
     }
@@ -79,7 +76,7 @@ window.addEventListener("load", function () {
       document.getElementById("tpm-description-bgt").innerHTML =
         response.weather[0].description.charAt(0).toUpperCase() + response.weather[0].description.slice(1);
 
-      var img = document.createElement("img");
+      let img = document.createElement("img");
       img.src = `./img/icons/${response.weather[0].icon}.png`;
       document.getElementById("img-container-bgt").appendChild(img);
     }
@@ -94,7 +91,7 @@ window.addEventListener("load", function () {
       document.getElementById("tpm-description-paris").innerHTML =
         response.weather[0].description.charAt(0).toUpperCase() + response.weather[0].description.slice(1);
 
-      var img = document.createElement("img");
+      let img = document.createElement("img");
       img.src = `./img/icons/${response.weather[0].icon}.png`;
       document.getElementById("img-container-paris").appendChild(img);
     }
